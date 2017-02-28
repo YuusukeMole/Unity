2152d32f48306b99284676beec13150b09339f93
LAPTOP-TAFL9MMM\syuu1 <s.yuu12.nawate@gmail.com>
2017-02-28 18:12:57 +0900

    メッセージの出力を追加

---
+++ Assets/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour 
+{
+
+	void Start () {
+		Debug.Log("Hello GitHub");
+	}
+
+}
